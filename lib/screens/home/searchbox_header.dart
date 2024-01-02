071eef7779d9d0d76b3955ccd8f0951537db7f54
Sandesh Hamal Thakuri <sandeshhamal5890@gmail.com>
2024-01-02 17:00:28 +0545

    Refactored

--- lib/screens/home/searchbox_header.dart
+++ lib/screens/home/searchbox_header.dart
@@ -34,6 +34,7 @@ class _SearchBoxHeaderState extends State<SearchBoxHeader> {
   @override
   Widget build(BuildContext context) {
     double paddingSize = MediaQuery.of(context).size.width * 0.04;
+    double screenWidth = MediaQuery.of(context).size.width;
 
     return Container(
       margin: const EdgeInsets.only(bottom: 24),
@@ -65,81 +66,82 @@ class _SearchBoxHeaderState extends State<SearchBoxHeader> {
                       }
                     },
                   ),
-                  address == 'No Location'
-                      ? const CircularProgressIndicator()
-                      : SizedBox(
-                          width: MediaQuery.of(context).size.width < 420
-                              ? 280
-                              : 550,
-                          child: address == 'No Location Provided'
-                              ? InkWell(
-                                  onTap: () {
-                                    value.getLocationPermission();
-                                  },
-                                  child: Text(
-                                    address,
-                                    style: addStyle,
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                )
-                              : InkWell(
-                                  onTap: () {
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: ((context) =>
-                                            const CurrentLocation()),
-                                      ),
-                                    );
-                                  },
-                                  child: Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      const Text(
-                                        'Delivery to:',
-                                        style: addStyle,
-                                      ),
-                                      Expanded(
-                                        child: SizedBox(
-                                          width: MediaQuery.of(context)
-                                                      .size
-                                                      .width <
-                                                  420
-                                              ? 350
-                                              : 550,
-                                          child:  Marquee(
-                                                  startAfter: const Duration(
-                                                      seconds: 2),
-                                                  text: address,
-                                                  style: addStyle,
-                                                  scrollAxis: Axis.horizontal,
-                                                  crossAxisAlignment:
-                                                      CrossAxisAlignment.center,
-                                                  blankSpace: 5.0,
-                                                  velocity: 50.0,
-                                                  pauseAfterRound:
-                                                      const Duration(
-                                                          seconds: 1),
-                                                  accelerationDuration:
-                                                      const Duration(
-                                                          seconds: 1),
-                                                  accelerationCurve:
-                                                      Curves.linear,
-                                                  decelerationDuration:
-                                                      const Duration(
-                                                          milliseconds: 500),
-                                                  decelerationCurve:
-                                                      Curves.easeOut,
-                                                )
+                  _buildAddressWidget(context, value,screenWidth),
+                  // address == 'No Location'
+                  //     ? const CircularProgressIndicator()
+                  //     : SizedBox(
+                  //         width: MediaQuery.of(context).size.width < 420
+                  //             ? 280
+                  //             : 550,
+                  //         child: address == 'No Location Provided'
+                  //             ? InkWell(
+                  //                 onTap: () {
+                  //                   value.getLocationPermission();
+                  //                 },
+                  //                 child: Text(
+                  //                   address,
+                  //                   style: addStyle,
+                  //                   overflow: TextOverflow.ellipsis,
+                  //                 ),
+                  //               )
+                  //             : InkWell(
+                  //                 onTap: () {
+                  //                   Navigator.push(
+                  //                     context,
+                  //                     MaterialPageRoute(
+                  //                       builder: ((context) =>
+                  //                           const CurrentLocation()),
+                  //                     ),
+                  //                   );
+                  //                 },
+                  //                 child: Column(
+                  //                   mainAxisSize: MainAxisSize.min,
+                  //                   crossAxisAlignment:
+                  //                       CrossAxisAlignment.start,
+                  //                   children: [
+                  //                     const Text(
+                  //                       'Delivery to:',
+                  //                       style: addStyle,
+                  //                     ),
+                  //                     Expanded(
+                  //                       child: SizedBox(
+                  //                         width: MediaQuery.of(context)
+                  //                                     .size
+                  //                                     .width <
+                  //                                 420
+                  //                             ? 350
+                  //                             : 550,
+                  //                         child:  Marquee(
+                  //                                 startAfter: const Duration(
+                  //                                     seconds: 2),
+                  //                                 text: address,
+                  //                                 style: addStyle,
+                  //                                 scrollAxis: Axis.horizontal,
+                  //                                 crossAxisAlignment:
+                  //                                     CrossAxisAlignment.center,
+                  //                                 blankSpace: 5.0,
+                  //                                 velocity: 50.0,
+                  //                                 pauseAfterRound:
+                  //                                     const Duration(
+                  //                                         seconds: 1),
+                  //                                 accelerationDuration:
+                  //                                     const Duration(
+                  //                                         seconds: 1),
+                  //                                 accelerationCurve:
+                  //                                     Curves.linear,
+                  //                                 decelerationDuration:
+                  //                                     const Duration(
+                  //                                         milliseconds: 500),
+                  //                                 decelerationCurve:
+                  //                                     Curves.easeOut,
+                  //                               )
                                               
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                        ),
+                  //                       ),
+                  //                     ),
+                  //                   ],
+                  //                 ),
+                  //               ),
+                  //       ),
                   const Spacer(),
                   IconButton(
                     onPressed: () {},
@@ -154,56 +156,57 @@ class _SearchBoxHeaderState extends State<SearchBoxHeader> {
             bottom: 0,
             left: 0,
             right: 0,
-            child: Container(
-              margin: EdgeInsets.symmetric(horizontal: paddingSize),
-              height: 44,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                    offset: const Offset(0, 3),
-                    blurRadius: 5,
-                    color: Colors.black.withOpacity(0.5),
-                  ),
-                ],
-              ),
-              child: Center(
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Expanded(
-                      child: TextField(
-                        onChanged: (value) {},
-                        decoration: InputDecoration(
-                          contentPadding:
-                              const EdgeInsets.symmetric(vertical: 10),
-                          hintText: "Search food or restaurant",
-                          hintStyle: TextStyle(
-                            fontFamily: 'Inter',
-                            fontWeight: FontWeight.w400,
-                            fontSize: 13,
-                            color: txtColor.withOpacity(0.5),
-                          ),
-                          enabledBorder: InputBorder.none,
-                          focusedBorder: InputBorder.none,
-                          prefixIcon: IconButton(
-                            constraints: const BoxConstraints(),
-                            icon: const Icon(
-                              Icons.search,
-                              color: Colors.black,
-                              size: 24,
-                            ),
-                            onPressed: () {},
-                          ),
-                          prefixIconColor: Colors.black,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
+            child: _buildSearchBox(paddingSize),
+            // Container(
+            //   margin: EdgeInsets.symmetric(horizontal: paddingSize),
+            //   height: 44,
+            //   decoration: BoxDecoration(
+            //     color: Colors.white,
+            //     borderRadius: BorderRadius.circular(10),
+            //     boxShadow: [
+            //       BoxShadow(
+            //         offset: const Offset(0, 3),
+            //         blurRadius: 5,
+            //         color: Colors.black.withOpacity(0.5),
+            //       ),
+            //     ],
+            //   ),
+            //   child: Center(
+            //     child: Row(
+            //       crossAxisAlignment: CrossAxisAlignment.center,
+            //       children: <Widget>[
+            //         Expanded(
+            //           child: TextField(
+            //             onChanged: (value) {},
+            //             decoration: InputDecoration(
+            //               contentPadding:
+            //                   const EdgeInsets.symmetric(vertical: 10),
+            //               hintText: "Search food or restaurant",
+            //               hintStyle: TextStyle(
+            //                 fontFamily: 'Inter',
+            //                 fontWeight: FontWeight.w400,
+            //                 fontSize: 13,
+            //                 color: txtColor.withOpacity(0.5),
+            //               ),
+            //               enabledBorder: InputBorder.none,
+            //               focusedBorder: InputBorder.none,
+            //               prefixIcon: IconButton(
+            //                 constraints: const BoxConstraints(),
+            //                 icon: const Icon(
+            //                   Icons.search,
+            //                   color: Colors.black,
+            //                   size: 24,
+            //                 ),
+            //                 onPressed: () {},
+            //               ),
+            //               prefixIconColor: Colors.black,
+            //             ),
+            //           ),
+            //         ),
+            //       ],
+            //     ),
+            //   ),
+            // ),
           ),
         ],
       ),
@@ -251,3 +254,121 @@ class _CurrentLocationState extends State<CurrentLocation> {
     );
   }
 }
+
+  Widget _buildAddressWidget(BuildContext context, LocationProvider value,double screenWidth) {
+    String address = value.address;
+    return address == 'No Location'
+        ? const CircularProgressIndicator()
+        : SizedBox(
+            width: screenWidth < 420 ? 280 : 550,
+            child: address == 'No Location Provided'
+                ? InkWell(
+                    onTap: () {
+                      value.getLocationPermission();
+                    },
+                    child: Text(
+                      address,
+                      style: addStyle,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  )
+                : InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: ((context) => const CurrentLocation()),
+                        ),
+                      );
+                    },
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Delivery to:',
+                          style: addStyle,
+                        ),
+                        Expanded(
+                          child: SizedBox(
+                            width: screenWidth < 420
+                                ? 350
+                                : 550,
+                            child: Marquee(
+                              startAfter: const Duration(seconds: 2),
+                              text: address,
+                              style: addStyle,
+                              scrollAxis: Axis.horizontal,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              blankSpace: 5.0,
+                              velocity: 50.0,
+                              pauseAfterRound: const Duration(seconds: 1),
+                              accelerationDuration: const Duration(seconds: 1),
+                              accelerationCurve: Curves.linear,
+                              decelerationDuration:
+                                  const Duration(milliseconds: 500),
+                              decelerationCurve: Curves.easeOut,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+          );
+  }
+
+
+  Widget _buildSearchBox(double paddingSize) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: paddingSize),
+      height: 44,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            offset: const Offset(0, 3),
+            blurRadius: 5,
+            color: Colors.black.withOpacity(0.5),
+          ),
+        ],
+      ),
+      child: Center(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              child: TextField(
+                onChanged: (value) {},
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.symmetric(vertical: 10),
+                  hintText: "Search food or restaurant",
+                  hintStyle: TextStyle(
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w400,
+                    fontSize: 13,
+                    color: txtColor.withOpacity(0.5),
+                  ),
+                  enabledBorder: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                  prefixIcon: IconButton(
+                    constraints: const BoxConstraints(),
+                    icon: const Icon(
+                      Icons.search,
+                      color: Colors.black,
+                      size: 24,
+                    ),
+                    onPressed: () {},
+                  ),
+                  prefixIconColor: Colors.black,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+
