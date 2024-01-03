6394ece65cdf19cd2f8ed3ae976525f8b2cda009
Sandesh Hamal Thakuri <sandeshhamal5890@gmail.com>
2024-01-03 16:19:18 +0545

    Refactored irrelevant use of media query in pupular.dart and made location.dart more modular

--- lib/provider/location.dart
+++ lib/provider/location.dart
@@ -5,8 +5,8 @@ import 'package:http/http.dart' as http;
 import 'package:open_street_map_search_and_pick/open_street_map_search_and_pick.dart';
 
 class LocationProvider with ChangeNotifier {
-  List<String> _addresses = [];
-  List<String> get addresses => _addresses;
+  List<String> _addressList = [];
+  List<String> get addresses => _addressList;
 
   String _address = 'No Location';
   String get address => _address;
@@ -20,7 +20,7 @@ class LocationProvider with ChangeNotifier {
   }
 
   void setAddresses(List<String> addresses) {
-    _addresses = addresses;
+    _addressList = addresses;
     notifyListeners();
   }
 
@@ -35,16 +35,16 @@ class LocationProvider with ChangeNotifier {
 
       if (permission == LocationPermission.denied ||
           permission == LocationPermission.deniedForever) {
-        LocationPermission permission = await Geolocator.requestPermission();
-        if (permission == LocationPermission.denied ||
-            permission == LocationPermission.deniedForever) {
-          setAddress('No Location Provided');
-        } else {
-          await getCurrentPosition();
-        }
+        permission = await Geolocator.requestPermission();
+      }
+
+      if (permission == LocationPermission.denied ||
+          permission == LocationPermission.deniedForever) {
+        setAddress('No Location Provided');
       } else {
         await getCurrentPosition();
       }
+
       notifyListeners();
     } catch (e) {
       print('Error getting location permission: $e');
@@ -53,103 +53,55 @@ class LocationProvider with ChangeNotifier {
     }
   }
 
-  // Future<void> getLocationPermission() async {
-  //   LocationPermission permission = await Geolocator.checkPermission();
-
-  //   if (permission == LocationPermission.denied ||
-  //       permission == LocationPermission.deniedForever) {
-  //     // print("Permission not given");
-  //     LocationPermission permission = await Geolocator.requestPermission();
-  //     if (permission == LocationPermission.denied ||
-  //         permission == LocationPermission.deniedForever) {
-  //       setAddress('No Location Provided');
-
-  //       //_address = 'No Location Provided';
-  //     } else {
-  //       getCurrentPosition();
-  //     }
-  //   } else {
-  //     getCurrentPosition();
-  //   }
-  //   notifyListeners();
-  // }
-
   Future<void> getCurrentPosition() async {
     try {
       Position position = await Geolocator.getCurrentPosition(
           desiredAccuracy: LocationAccuracy.best);
-      setAddress(await getAddress(position.latitude, position.longitude));
-      //_address = await getAddress(position.latitude, position.longitude);
+      String newAddress =
+          await getAddress(position.latitude, position.longitude);
+      setAddress(newAddress);
+
       setCurrentPosition(LatLong(position.latitude, position.longitude));
-      //_currentPosition = LatLong(position.latitude, position.longitude);
     } catch (e) {
       print('Error getting current position: $e');
       setAddress('No Location Provided');
     }
     notifyListeners();
-
-    //Position position = await Geolocator.getCurrentPosition(
-    //desiredAccuracy: LocationAccuracy.best);
-    //setAddress(await getAddress(position.latitude, position.longitude));
-    //_address = await getAddress(position.latitude, position.longitude);
-    //setCurrentPosition(LatLong(position.latitude, position.longitude));
-    //_currentPosition = LatLong(position.latitude, position.longitude);
   }
 
-  // Future<String> getAddress(double latitude, double longitude) async {
-  //   try {
-  //     final apiUrl =
-  //         'https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=$latitude&lon=$longitude&zoom=18&addressdetails=1';
+  Future<String> getAddress(double latitude, double longitude) async {
+    try {
+      final apiUrl =
+          'https://nominatim.openstreetmap.org/reverse?format=json&lat=$latitude&lon=$longitude&zoom=18&addressdetails=1';
 
-  //     // https://nominatim.openstreetmap.org/reverse?lat=<value>&lon=<value>&<params>
+      final response = await http.get(Uri.parse(apiUrl));
 
-  //     final response = await http.get(Uri.parse(apiUrl));
+      if (response.statusCode == 200) {
+        return parseAddressResponse(response.body);
+      } else {
+        return 'Failed to get address. Status code: ${response.statusCode}';
+      }
+      // When dealing with the API response, it's a good practice to check if the key(display_name) exists before accessing it
+    } catch (e) {
+      print('Error: $e');
+      return 'No Location Provided';
+    }
+  }
 
-  //     if (response.statusCode == 200) {
-  //       final data = json.decode(response.body);
-  //       print(data);
-
-  //       String address = data['display_name'];
-  //      // print(address);
-  //       return address;
-  //     } else {
-  //       print('Error: ${response.statusCode}');
-  //       return 'No Location Provided';
-  //     }
-  //   } catch (e) {
-  //     print('Error: $e');
-  //     return 'No Location Provided';
-  //   }
-  // }
-
-Future<String> getAddress(double latitude, double longitude) async {
-  try {
-    final apiUrl =
-        'https://nominatim.openstreetmap.org/reverse?format=json&lat=$latitude&lon=$longitude&zoom=18&addressdetails=1';
-
-    final response = await http.get(Uri.parse(apiUrl));
-
-    if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-      //print(data);
-      // print(data['display_name']);
+  String parseAddressResponse(String responseBody) {
+    try {
+      final data = json.decode(responseBody);
 
       // Check if 'display_name' exists and is not null
       if (data != null && data['display_name'] != null) {
-
-        String address = data['display_name'];
-        return address;
+        return data['display_name'];
       } else {
         return 'Invalid API Response';
       }
-    } else {
-      return 'Failed to get address. Status code: ${response.statusCode}';
+      // When dealing with the API response, it's a good practice to check if the key exists before accessing it.
+    } catch (e) {
+      print('Error parsing API response: $e');
+      return 'Invalid API Response';
     }
-  } catch (e) {
-    print('Error: $e');
-    return 'No Location Provided';
   }
-}
-
-
 }
--- lib/screens/home/popular/popular.dart
+++ lib/screens/home/popular/popular.dart
@@ -7,19 +7,26 @@ import 'package:provider/provider.dart';
 import '../section_header.dart';
 
 class PopularRestaurants extends StatelessWidget {
-  const PopularRestaurants({super.key});
+  // When you use super.key, you're explicitly passing the key to the superclass (the StatelessWidget constructor). This syntax is useful when you want to forward the key to the superclass without modifications.
+  // When you use Key? key directly in the constructor parameters, you're declaring a nullable Key named key. This gives you flexibility in how you handle the key within the widget.
+  // In practice, both methods achieve the same result. However, using Key? key directly in the constructor parameters is more explicit and may be considered a cleaner approach. It's a matter of personal preference and coding style.
+
+  //const PopularRestaurants({super.key});
+  const PopularRestaurants({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
     return Column(
       children: [
         Padding(
           padding: EdgeInsets.symmetric(
-              horizontal: MediaQuery.of(context).size.width * 0.04), // 16.4571
+              horizontal: screenWidth * 0.04), // 16.4571
           child: SectionHeader(
+            key: UniqueKey(),
             headerText: 'Popular In Your Area',
             viewText: 'See More  >',
-            viewWidth: MediaQuery.of(context).size.width *
+            viewWidth: screenWidth *
                 0.2, // 0.196 i.e (80.6399 and overflow nahuna greater than 81 huna parxa so 0.2(82.2857) ) ma overflow janxa (Previously 87 thyo )
             onViewClick: () {
               Navigator.push(
@@ -31,7 +38,7 @@ class PopularRestaurants extends StatelessWidget {
             },
           ),
         ),
-        const PopularRestSlider(),
+        _buildPopularRestSlider(context),
         Divider(
           thickness: 2.0,
           color: const Color(0xFFF3F3F3).withOpacity(0.8),
@@ -41,22 +48,65 @@ class PopularRestaurants extends StatelessWidget {
     );
   }
 }
+// In your case, the PopularRestSlider widget is not too large, and it doesn't seem to have complex logic or state. Therefore, separating it into a function within the same file (PopularRestaurants) may be a reasonable choice. If the widget grows in complexity or if you find yourself reusing it in different parts of your application, you might consider creating a separate class for it. So i will be commenting this code and make a function  which provides the same implementation
 
-class PopularRestSlider extends StatelessWidget {
-  const PopularRestSlider({super.key});
+// class PopularRestSlider extends StatelessWidget {
+//   const PopularRestSlider({super.key});
 
-  @override
-  Widget build(BuildContext context) {
+//   @override
+//   Widget build(BuildContext context) {
+//     double screenWidth = MediaQuery.of(context).size.width;
+//     double sliderWidth;
+//     if (screenWidth > 420) {
+//       sliderWidth = 420;
+//     } else {
+//       sliderWidth = screenWidth;
+//     }
+
+//     double sliderHeight =
+//         sliderWidth * (9 / 16) + 40; // (231.428 + 30 = 261.428) (30 at first)
+
+//     return Container(
+//       margin: const EdgeInsets.only(top: 14, bottom: 5),
+//       height: sliderHeight,
+//       child: Consumer<RestaurantModel>(
+//         builder: (context, value, child) {
+//           int count = value.restaurants.length;
+//           return CarouselSlider.builder(
+//             itemCount: count > 4 ? 4 : count,
+//             itemBuilder: (context, index, realIdx) {
+//               return Padding(
+//                 padding: const EdgeInsets.only(right: 8.0),
+//                 child: RestaurantView(
+//                   sliderHeight: sliderHeight - 10,
+//                   restaurant: value.restaurants[index],
+//                   sliderWidth: sliderWidth,
+//                 ),
+//               );
+//             },
+//             options: CarouselOptions(
+//               aspectRatio: 16 / 9,
+//               viewportFraction: sliderWidth * 0.92 / screenWidth,
+//               initialPage: 0,
+//               enableInfiniteScroll: true,
+//               autoPlay: true,
+//               height: sliderHeight,
+//               disableCenter: true,
+//               autoPlayInterval: const Duration(seconds: 5),
+//               autoPlayAnimationDuration: const Duration(milliseconds: 2000),
+//               autoPlayCurve: Curves.fastOutSlowIn,
+//             ),
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
+
+Widget _buildPopularRestSlider(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
-    double sliderWidth;
-    if (screenWidth > 420) {
-      sliderWidth = 420;
-    } else {
-      sliderWidth = screenWidth;
-    }
-
-    double sliderHeight =
-        sliderWidth * (9 / 16) + 40; // (231.428 + 30 = 261.428) (30 at first)
+    double sliderWidth = screenWidth > 420 ? 420 : screenWidth;
+    double sliderHeight = sliderWidth * (9 / 16) + 40;
 
     return Container(
       margin: const EdgeInsets.only(top: 14, bottom: 5),
@@ -93,4 +143,4 @@ class PopularRestSlider extends StatelessWidget {
       ),
     );
   }
-}
+
