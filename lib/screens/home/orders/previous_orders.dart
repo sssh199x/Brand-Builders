01ea0526d8572a2d8ccb7dcca7e2cb0b207c46c1
Sandesh Hamal Thakuri <sandeshhamal5890@gmail.com>
2024-01-07 16:09:13 +0545

    Refactored previous_orders.dart

--- lib/screens/home/orders/previous_orders.dart
+++ lib/screens/home/orders/previous_orders.dart
@@ -9,60 +9,58 @@ import 'order_view.dart';
 class PreviousOrders extends StatelessWidget {
   const PreviousOrders({super.key});
 
-  Widget _buildHeader (BuildContext context) { 
+  Widget _buildSectionHeader(BuildContext context) {
+    double scrWidth = MediaQuery.of(context).size.width;
     return Padding(
-          padding: EdgeInsets.symmetric(
-              horizontal: MediaQuery.of(context).size.width * 0.04),
-          child: SectionHeader(
-            headerText: 'Your Previous Orders',
-            viewText: 'View All  >',
-            viewWidth: MediaQuery.of(context).size.width * 0.2,
-            onViewClick: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: ((context) => const AllOrders()),
+      padding: EdgeInsets.symmetric(horizontal: scrWidth * 0.04),
+      child: SectionHeader(
+        headerText: 'Your Previous Orders',
+        viewText: 'View All  >',
+        viewWidth: scrWidth * 0.2,
+        onViewClick: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: ((context) => const AllOrders()),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildCarouselSlider(
+      double sliderWidth, double sliderHeight, double screenWidth) {
+    return SizedBox(
+      height: sliderHeight,
+      child: Consumer<FoodModel>(builder: (context, value, child) {
+        int count = value.foodItems.length;
+        return CarouselSlider.builder(
+            itemCount: count > 4 ? 4 : count,
+            itemBuilder: (context, index, realIdx) {
+              return Padding(
+                padding: const EdgeInsets.only(right: 8.0),
+                child: OrderView(
+                  orderItem: value.foodItems[index],
+                  sliderHeight: sliderHeight - 10,
+                  sliderWidth: sliderWidth,
                 ),
               );
             },
-          ),
-        );
+            options: CarouselOptions(
+              height: sliderHeight,
+              aspectRatio: 16 / 9,
+              viewportFraction: sliderWidth * 0.92 / screenWidth,
+              initialPage: 0,
+              enableInfiniteScroll: true,
+              autoPlay: true,
+              autoPlayInterval: const Duration(seconds: 5),
+              autoPlayAnimationDuration: const Duration(milliseconds: 800),
+              autoPlayCurve: Curves.fastOutSlowIn,
+            ));
+      }),
+    );
   }
-
-  Widget _buildCarouselSlider(double sliderWidth,double sliderHeight,double screenWidth) {
-    return         SizedBox(
-          height: sliderHeight,
-          child: Consumer<FoodModel>(builder: (context, value, child) {
-            int count = value.foodItems.length;
-            return CarouselSlider.builder(
-                itemCount: count > 4 ? 4 : count,
-                itemBuilder: (context, index, realIdx) {
-                  return Padding(
-                    padding: const EdgeInsets.only(right: 8.0),
-                    child: OrderView(
-                      orderItem: value.foodItems[index],
-                      sliderHeight: sliderHeight - 10,
-                      sliderWidth: sliderWidth,
-                    ),
-                  );
-                },
-                options: CarouselOptions(
-                  height: sliderHeight,
-                  aspectRatio: 16 / 9,
-                  viewportFraction: sliderWidth * 0.92 / screenWidth,
-                  initialPage: 0,
-                  enableInfiniteScroll: true,
-                  autoPlay: true,
-                  autoPlayInterval: const Duration(seconds: 5),
-                  autoPlayAnimationDuration: const Duration(milliseconds: 800),
-                  autoPlayCurve: Curves.fastOutSlowIn,
-                ));
-          }),
-        );
-
-  }
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -72,8 +70,11 @@ class PreviousOrders extends StatelessWidget {
 
     return Column(
       children: [
-        _buildHeader(context),
-       _buildCarouselSlider(sliderWidth, sliderHeight, screenWidth),
+        _buildSectionHeader(context),
+        const SizedBox(
+          height: 10,
+        ),
+        _buildCarouselSlider(sliderWidth, sliderHeight, screenWidth),
         const SizedBox(height: 24),
       ],
     );
