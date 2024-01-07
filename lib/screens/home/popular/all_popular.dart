094776a50ab25f08a5190085da285b6b5f6daec5
Sandesh Hamal Thakuri <sandeshhamal5890@gmail.com>
2024-01-07 16:09:32 +0545

    Refactored all_popular.dart

--- lib/screens/home/popular/all_popular.dart
+++ lib/screens/home/popular/all_popular.dart
@@ -6,24 +6,26 @@ import 'package:provider/provider.dart';
 
 class PopularAll extends StatelessWidget {
   const PopularAll({super.key});
-
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(title: const Text('Popular'),backgroundColor: prmColor,foregroundColor: bgColor),
+      appBar: AppBar(
+          title: const Text('Popular'),
+          backgroundColor: prmColor,
+          foregroundColor: bgColor),
       body: Consumer<RestaurantModel>(
         builder: (context, value, child) {
           return ListView.builder(
             itemCount: value.restaurants.length,
             itemBuilder: (context, index) {
               return Padding(
-                padding: const EdgeInsets.all(15.0),
+                padding: const EdgeInsets.all(8.0),
                 child: RestaurantView(
                   restaurant: value.restaurants[index],
                   sliderHeight: screenWidth *
-                      (9 / 16), // (9/16 = 0.5625, 411.42857 * 0.5625 = 231.428)
-                  sliderWidth: screenWidth,
+                      (9 / 16) + 50, // (9/16 = 0.5625, 411.42857 * 0.5625 = 231.428)
+                  sliderWidth: double.infinity,
                 ),
               );
             },
