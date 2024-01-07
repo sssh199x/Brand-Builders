9bacff48a7f8cd5225f906ff019389a8b388dfe2
Sandesh Hamal Thakuri <sandeshhamal5890@gmail.com>
2024-01-07 16:07:45 +0545

    Refactored favourites.dart

--- lib/screens/home/favourites/favourites.dart
+++ lib/screens/home/favourites/favourites.dart
@@ -8,48 +8,61 @@ import 'favourite_view.dart';
 class FavouriteRestaurants extends StatelessWidget {
   const FavouriteRestaurants({super.key});
 
-  @override
-  Widget build(BuildContext context) {
+  Widget _buildSectionHeader(BuildContext context) {
+    double scrWidth = MediaQuery.of(context).size.width;
+    return SectionHeader(
+        headerText: 'Your Favourite Restaurants',
+        viewText: 'View All  >',
+        viewWidth: scrWidth * 0.2,
+        onViewClick: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: ((context) => const AllFavourites()),
+            ),
+          );
+        });
+  }
+
+  Widget _buildFavouriteRestaurantList(BuildContext context) {
     double scrWidth = MediaQuery.of(context).size.width;
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: scrWidth * 0.04),
+      padding: const EdgeInsets.only(bottom: 16),
+      height: scrWidth < 420 ? scrWidth / 1.8701 : scrWidth / 1.8,
+      child: Consumer<RestaurantModel>(builder: (context, value, child) {
+        int count = value.restaurants.length;
+        return ListView.builder(
+          padding: EdgeInsets.zero,
+          shrinkWrap: true,
+          scrollDirection: Axis.horizontal,
+          itemCount: count > 6 ? 6 : count,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: FavouriteView(
+                favItem: value.restaurants[index],
+                sliderWidth: scrWidth - 170,
+                sliderHeight: scrWidth * 9 / 16 - 20,
+              ),
+            );
+          },
+        );
+      }),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+          horizontal: MediaQuery.of(context).size.width * 0.04),
       child: Column(
         children: [
-          SectionHeader(
-              headerText: 'Your Favourite Restaurants',
-              viewText: 'View All  >',
-              viewWidth: scrWidth * 0.2,
-              onViewClick: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: ((context) => const AllFavourites()
-                    ),
-                  ),
-                );
-              }),
-          Container(
-            padding: const EdgeInsets.only(bottom: 16),
-            height:  scrWidth < 420 ? scrWidth/ 1.8701 :  scrWidth / 1.8,
-            child: Consumer<RestaurantModel>(builder: (context, value, child) {
-              int count = value.restaurants.length;
-              return ListView.builder(
-                padding: EdgeInsets.zero,
-                shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
-                itemCount: count > 6 ? 6 : count,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.only(right: 8.0),
-                    child: FavouriteView(favItem: value.restaurants[index],
-                    sliderWidth: scrWidth < 420 ? scrWidth - 80 : scrWidth - 150,
-                    sliderHeight: scrWidth < 420 ? scrWidth * 9/16 : scrWidth * 9/16  ,
-                    ),
-                  );
-                },
-              );
-            }),
+          _buildSectionHeader(context),
+          const SizedBox(
+            height: 10,
           ),
+          _buildFavouriteRestaurantList(context),
         ],
       ),
     );
