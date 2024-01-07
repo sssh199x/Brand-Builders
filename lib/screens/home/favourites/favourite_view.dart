62b08fb8b38d2ddc1589d231a765cf531e0d1931
Sandesh Hamal Thakuri <sandeshhamal5890@gmail.com>
2024-01-07 16:07:28 +0545

    Refactored favourite_view.dart

--- lib/screens/home/favourites/favourite_view.dart
+++ lib/screens/home/favourites/favourite_view.dart
@@ -4,47 +4,73 @@ import 'package:ordering_app/screens/about_restaurant.dart';
 import 'package:ordering_app/provider/restaurant_model.dart';
 
 class FavouriteView extends StatelessWidget {
-  const FavouriteView({super.key, required this.favItem, required this.sliderWidth, required this.sliderHeight});
+  const FavouriteView({
+    super.key,
+    required this.favItem,
+    required this.sliderWidth,
+    required this.sliderHeight,
+  });
 
   final Restaurant favItem;
   final double sliderWidth;
   final double sliderHeight;
+
+  Widget _buildImageContainer() {
+    return SizedBox(
+      width: sliderWidth,
+      height: sliderHeight - 70,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: Image.asset(favItem.image, fit: BoxFit.cover),
+      ),
+    );
+  }
+
+  Widget _buildTitle() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(favItem.name, style: titleStyle),
+        const SizedBox(
+          height: 8,
+        )
+      ],
+    );
+  }
+
+  Widget _buildRatingAndDistance() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        const Icon(Icons.star, color: starColor, size: 15),
+        const SizedBox(width: 6),
+        Text(favItem.rating.toString(), style: txtStyle1),
+        Text(' (${favItem.reviews.toString()})', style: txtStyle2),
+        const SizedBox(width: 15),
+        const Icon(Icons.location_on, size: 12),
+        const SizedBox(width: 6),
+        Text('${favItem.distance.toString()} km', style: txtStyle2),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
         Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: ((context) => AboutRestaurant(restaurant: favItem))));
+          context,
+          MaterialPageRoute(
+            builder: (context) => AboutRestaurant(restaurant: favItem),
+          ),
+        );
       },
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Container(
-              padding: const EdgeInsets.symmetric(vertical: 16),
-              width: sliderWidth,
-              height: sliderHeight - 70,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Image.asset(favItem.image, fit: BoxFit.cover),
-              )),
-          Text(favItem.name, style: titleStyle),
-          // const SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              const Icon(Icons.star, color: starColor, size: 15),
-              const SizedBox(width: 6),
-              Text(favItem.rating.toString(), style: txtStyle1),
-              Text(' (${favItem.reviews.toString()})', style: txtStyle2),
-              const SizedBox(width: 15),
-              const Icon(Icons.location_on, size: 12),
-              const SizedBox(width: 6),
-              Text('${favItem.distance.toString()} km', style: txtStyle2),
-            ],
-          ),
+          _buildImageContainer(),
+          _buildTitle(),
+          _buildRatingAndDistance(),
         ],
       ),
     );
