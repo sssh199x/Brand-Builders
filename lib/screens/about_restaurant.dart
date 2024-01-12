5b512a8fe67384d5304101021c4b0f4022fa5d66
Sandesh Hamal Thakuri <sandeshhamal5890@gmail.com>
2024-01-12 16:13:47 +0545

    Refactored about_restaurant

--- lib/screens/about_restaurant.dart
+++ lib/screens/about_restaurant.dart
@@ -3,6 +3,7 @@ import 'package:marquee/marquee.dart';
 import 'package:ordering_app/provider/restaurant_model.dart';
 import 'package:ordering_app/res/constants.dart';
 import 'package:ordering_app/screens/food_info.dart';
+import 'package:ordering_app/screens/portrait_landscape_stack.dart';
 import 'package:ordering_app/screens/restaurant_info.dart';
 
 class AboutRestaurant extends StatefulWidget {
@@ -28,106 +29,6 @@ class _AboutRestaurantState extends State<AboutRestaurant>
   void dispose() {
     _tabController.dispose();
     super.dispose();
-  }
-
-  Widget _buildPortraitLandscapeStack(double screenWidth, BuildContext context,
-      double paddingSize, Orientation orientation) {
-    return Stack(
-      children: [
-        Positioned(
-          top: 0,
-          left: 0,
-          right: 0,
-          child: Align(
-            child: SizedBox(
-              width: double.infinity,
-              height: orientation == Orientation.portrait
-                  ? screenWidth - 160
-                  : screenWidth - 720,
-              child: Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage(widget.restaurant.foodimage),
-                      fit: BoxFit.cover),
-                ),
-              ),
-            ),
-          ),
-        ),
-        Positioned(
-            left: 10,
-            top: orientation == Orientation.portrait
-                ? screenWidth / 2.1694
-                : screenWidth / 9.7733, //180 : 87,
-            child: Align(
-              child: SizedBox(
-                width: orientation == Orientation.portrait
-                    ? screenWidth / 5.1428
-                    : screenWidth / 13.7142, //i.e 80 : 62
-                height: orientation == Orientation.portrait
-                    ? screenWidth / 5.1428
-                    : screenWidth / 13.71425,
-                child: Container(
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      image: DecorationImage(
-                          image: AssetImage(widget.restaurant.image),
-                          fit: BoxFit.cover)),
-                ),
-              ),
-            )),
-        Positioned(
-            top: 10,
-            left: 10,
-            child: Align(
-              child: SizedBox(
-                width: orientation == Orientation.portrait
-                    ? screenWidth / 10.2857
-                    : screenWidth / 24.2938, // 40 : 35
-                height: orientation == Orientation.portrait
-                    ? screenWidth / 10.2857
-                    : screenWidth / 24.2938,
-                child: Container(
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    shape: BoxShape.circle,
-                  ),
-                  child: IconButton(
-                      iconSize:
-                          orientation == Orientation.portrait ? 24.0 : 15.0,
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      icon: const Icon(Icons.arrow_back_ios_new)),
-                ),
-              ),
-            )),
-        Positioned(
-            top: 10,
-            right: 10,
-            child: Align(
-              child: SizedBox(
-                width: orientation == Orientation.portrait
-                    ? screenWidth / 10.2857
-                    : screenWidth / 24.2938, // 40 : 35
-                height: orientation == Orientation.portrait
-                    ? screenWidth / 10.2857
-                    : screenWidth / 24.2938,
-                child: Container(
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    shape: BoxShape.circle,
-                  ),
-                  child: IconButton(
-                      iconSize:
-                          orientation == Orientation.portrait ? 24.0 : 15.0,
-                      onPressed: () {},
-                      icon: const Icon(Icons.search)),
-                ),
-              ),
-            )),
-      ],
-    );
   }
 
   Widget _buildRestaurantName(double paddingSize) {
@@ -637,8 +538,12 @@ class _AboutRestaurantState extends State<AboutRestaurant>
                       EdgeInsets.only(top: MediaQuery.of(context).padding.top),
                   width: double.infinity,
                   height: screenWidth - 140,
-                  child: _buildPortraitLandscapeStack(
-                      screenWidth, context, paddingSize, orientation),
+                  child: PortraitLandscapeStack(
+                      widget: widget,
+                      screenWidth: screenWidth,
+                      context: context,
+                      paddingSize: paddingSize,
+                      orientation: orientation),
                 ),
                 _buildsWholeRestaurantDescription(
                     paddingSize, screenWidth, orientation),
@@ -657,8 +562,12 @@ class _AboutRestaurantState extends State<AboutRestaurant>
                         top: MediaQuery.of(context).padding.top),
                     width: double.infinity,
                     height: screenWidth - 700,
-                    child: _buildPortraitLandscapeStack(
-                        screenWidth, context, paddingSize, orientation),
+                    child: PortraitLandscapeStack(
+                        widget: widget,
+                        screenWidth: screenWidth,
+                        context: context,
+                        paddingSize: paddingSize,
+                        orientation: orientation),
                   ),
                   _buildsWholeRestaurantDescription(
                       paddingSize, screenWidth, orientation),
