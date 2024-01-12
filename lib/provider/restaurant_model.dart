40d67237ff9f123e1b0de530726b1f6c541b4d7e
Sandesh Hamal Thakuri <sandeshhamal5890@gmail.com>
2024-01-12 16:13:13 +0545

    added new parameters location and contactno

--- lib/provider/restaurant_model.dart
+++ lib/provider/restaurant_model.dart
@@ -3,29 +3,38 @@ import 'package:flutter/foundation.dart';
 class RestaurantModel with ChangeNotifier {
   final restaurants = [
     Restaurant(
-      restaurantinfo: 'Serves meat, vegan options available. Creperie and eatery that offers a few vegan crepe options.' ,
+      restaurantinfo:
+          'Serves meat, vegan options available. Creperie and eatery that offers a few vegan crepe options.',
       name: 'French Crepie',
-      description: 'French, Cafe, European, Healthy, Vegetarian Friendly, Vegan Options',
+      description:
+          'French, Cafe, European, Healthy, Vegetarian Friendly, Vegan Options',
       image: 'assets/frenchcrepie.jpg',
       foodimage: 'assets/frenchcrepiefood1.jpeg',
       rating: 4.5,
       reviews: 232,
       distance: 1.3,
       time: 24,
+      location: 'Khahare Chowk, Lakeside',
+      contactno: '980-6667705',
     ),
     Restaurant(
-      restaurantinfo: 'The greatest pizza in town, with a huge selection of toppings.',
+      restaurantinfo:
+          'The greatest pizza in town, with a huge selection of toppings.',
       name: 'Godfathers Pizzeria Pvt.Ltd.',
-      description: 'Healthy and Hygienic fire wood pizza with the traditional wood fire oven.',
+      description:
+          'Healthy and Hygienic fire wood pizza with the traditional wood fire oven.',
       image: 'assets/godfather.jpg',
       foodimage: 'assets/pizza.jpg',
       rating: 4.5,
       reviews: 232,
       distance: 1.3,
       time: 24,
+      location: 'Lakeside Rd, Hallan Chowk 18th street',
+      contactno: '061-466501',
     ),
     Restaurant(
-      restaurantinfo: 'Flawless dining experience,consistency providing exceptional food & superlative service' ,
+      restaurantinfo:
+          'Flawless dining experience,consistency providing exceptional food & superlative service',
       name: 'Byanjan',
       description: 'Local cuisine, Indian, Contemporary, Nepali, International',
       image: 'assets/bhajyan.jpg',
@@ -34,6 +43,8 @@ class RestaurantModel with ChangeNotifier {
       reviews: 232,
       distance: 1.3,
       time: 24,
+      location: 'Lakeside Rd 15, Pokhara',
+      contactno: '061-456271',
     ),
     Restaurant(
       restaurantinfo: 'Cool vibe. Perfect ambience. Delicious food.',
@@ -44,37 +55,47 @@ class RestaurantModel with ChangeNotifier {
       rating: 4.5,
       reviews: 232,
       distance: 1.3,
-      time: 24,
+      time: 24, location: 'Hallan chowk ,Lakeside', contactno: '061-457421',
       // momos: ['Chicken Momo','Veg Momo'],
       // fooddescription: ['Chicken,Soup & Flour','Smashed Vegetables,Soup & Flour'],
       // foodprice: ['450','350'],
-
     ),
     Restaurant(
-      restaurantinfo: 'Serves meat, vegan options available. Creperie and eatery that offers a few vegan crepe options.',
+      restaurantinfo:
+          'Serves meat, vegan options available. Creperie and eatery that offers a few vegan crepe options.',
       name: 'French Crepie',
-      description: 'French, Cafe, European, Healthy, Vegetarian Friendly, Vegan Options',
+      description:
+          'French, Cafe, European, Healthy, Vegetarian Friendly, Vegan Options',
       image: 'assets/frenchcrepie.jpg',
       foodimage: 'assets/frenchcrepiefood1.jpeg',
       rating: 4.5,
       reviews: 232,
       distance: 1.3,
       time: 24,
+      location: 'Khahare Chowk, Lakeside',
+      contactno: '980-6667705',
     ),
   ];
 }
 
 class Restaurant {
-  final String name, image, description,restaurantinfo,foodimage;
+  final String name,
+      image,
+      description,
+      restaurantinfo,
+      foodimage,
+      location,
+      contactno;
   final double rating, distance;
   final int reviews, time;
   //final List momos,fooddescription,foodprice;
-  
 
-  Restaurant( {
+  Restaurant({
     // required this.momos,
     // required this.fooddescription,
     // required this.foodprice,
+    required this.contactno,
+    required this.location,
     required this.foodimage,
     required this.restaurantinfo,
     required this.description,
