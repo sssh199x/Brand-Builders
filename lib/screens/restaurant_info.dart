3429bf4458005ee55f758eb1e61c4d087288b899
Sandesh Hamal Thakuri <sandeshhamal5890@gmail.com>
2024-01-12 16:15:59 +0545

    added new lines of code for implementing google maps and the description of the restaurant

--- lib/screens/restaurant_info.dart
+++ lib/screens/restaurant_info.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ordering_app/provider/restaurant_model.dart';
+import 'package:ordering_app/res/constants.dart';
 import 'package:ordering_app/screens/map_utils.dart';
 
 class RestaurantInfo extends StatelessWidget {
@@ -9,11 +10,7 @@ class RestaurantInfo extends StatelessWidget {
   Widget _buildMapsContainer(BuildContext context, double paddingSize,
       double screenWidth, Orientation orientation) {
     return GestureDetector(
-      onTap: () => Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => const MapUtils(),
-          )),
+      onTap: () => MapUtils.openMap(28.2153, 83.9585),
       child: Container(
         margin: EdgeInsets.only(left: paddingSize, right: paddingSize),
         width: double.infinity,
@@ -21,7 +18,10 @@ class RestaurantInfo extends StatelessWidget {
             ? screenWidth - 250
             : screenWidth / 6,
         decoration: BoxDecoration(
-            color: Colors.black12, borderRadius: BorderRadius.circular(10)),
+          //image: const DecorationImage(image: AssetImage('')),
+          color: Colors.black12,
+          borderRadius: BorderRadius.circular(10),
+        ),
         child: Center(
           child: Container(
               width: orientation == Orientation.portrait ? 35 : 40,
@@ -40,7 +40,7 @@ class RestaurantInfo extends StatelessWidget {
   Widget build(BuildContext context) {
     double paddingSize = MediaQuery.of(context).size.width * 0.04;
     double screenWidth = MediaQuery.of(context).size.width;
-    final orientation = MediaQuery.of(context).orientation;
+    Orientation orientation = MediaQuery.of(context).orientation;
 
     return Scaffold(
       appBar: AppBar(
@@ -53,15 +53,127 @@ class RestaurantInfo extends StatelessWidget {
           ? Column(
               children: [
                 _buildMapsContainer(
-                    context, paddingSize, screenWidth, orientation)
+                    context, paddingSize, screenWidth, orientation),
+                const SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  margin:
+                      EdgeInsets.only(left: paddingSize, right: paddingSize),
+                  width: double.infinity,
+                  height: screenWidth - 100,
+                  // /color: Colors.black12,
+                  child: Column(
+                    children: [
+                      ListTileBuilder(
+                        restaurant: restaurant,
+                        restaurantName: restaurant.name,
+                        restaurantSubtitle: restaurant.location,
+                        leadingIcon: const Icon(Icons.location_city),
+                        trailingIconButton: IconButton(
+                            onPressed: () {},
+                            icon: const Icon(Icons.arrow_outward)),
+                      ),
+                      ListTileBuilder(
+                        restaurant: restaurant,
+                        restaurantName: restaurant.contactno,
+                        leadingIcon: const Icon(Icons.call),
+                        trailingIconButton: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.arrow_outward),
+                        ),
+                      ),
+                      ListTileBuilder(
+                          restaurant: restaurant,
+                          restaurantName: 'Delivery Time',
+                          leadingIcon: const Icon(Icons.timer),
+                          trailingIconButton: IconButton(
+                              onPressed: () {},
+                              icon: const Icon(Icons.expand_more))),
+                    ],
+                  ),
+                )
               ],
             )
-          : Column(
-              children: [
-                _buildMapsContainer(
-                    context, paddingSize, screenWidth, orientation)
-              ],
+          : SingleChildScrollView(
+              child: Column(
+                children: [
+                  _buildMapsContainer(
+                      context, paddingSize, screenWidth, orientation),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Container(
+                    margin:
+                        EdgeInsets.only(left: paddingSize, right: paddingSize),
+                    width: double.infinity,
+                    height: screenWidth / 3,
+                    //color: Colors.black12,
+                    child: Column(
+                      children: [
+                        ListTileBuilder(
+                          restaurant: restaurant,
+                          restaurantName: restaurant.name,
+                          restaurantSubtitle: restaurant.location,
+                          leadingIcon: const Icon(Icons.location_city),
+                          trailingIconButton: IconButton(
+                              onPressed: () {},
+                              icon: const Icon(Icons.arrow_outward)),
+                        ),
+                        ListTileBuilder(
+                          restaurant: restaurant,
+                          restaurantName: restaurant.contactno,
+                          leadingIcon: const Icon(Icons.call),
+                          trailingIconButton: IconButton(
+                            onPressed: () {},
+                            icon: const Icon(Icons.arrow_outward),
+                          ),
+                        ),
+                        ListTileBuilder(
+                            restaurant: restaurant,
+                            restaurantName: 'Delivery Time',
+                            leadingIcon: const Icon(Icons.timer),
+                            trailingIconButton: IconButton(
+                                onPressed: () {},
+                                icon: const Icon(Icons.expand_more))),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
+    );
+  }
+}
+
+class ListTileBuilder extends StatelessWidget {
+  const ListTileBuilder(
+      {super.key,
+      required this.restaurant,
+      required this.restaurantName,
+      this.restaurantSubtitle = '',
+      required this.leadingIcon,
+      required this.trailingIconButton});
+
+  final Restaurant restaurant;
+  final String restaurantName;
+  final String restaurantSubtitle;
+  final Widget leadingIcon;
+  final Widget trailingIconButton;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: leadingIcon,
+      title: Text(
+        restaurantName,
+        style: infoStyle,
+      ),
+      subtitle: Text(
+        restaurantSubtitle,
+      ),
+      trailing: trailingIconButton,
+      contentPadding: const EdgeInsets.only(right: 0),
     );
   }
 }
