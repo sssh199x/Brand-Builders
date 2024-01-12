10f580f559c61b9e7d595a715ec54b2106e02938
Sandesh Hamal Thakuri <sandeshhamal5890@gmail.com>
2024-01-12 16:14:15 +0545

    Added new lines of code for opening google map url

--- lib/screens/map_utils.dart
+++ lib/screens/map_utils.dart
@@ -1,10 +1,19 @@
-import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class MapUtils extends StatelessWidget {
-  const MapUtils({super.key});
+class MapUtils {
+  MapUtils._();
 
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold();
+  // Static Function : A  method that defines as a member of an object but is accesible directly from an API objects constructor,rather than from a object instance created via the constructor.
+
+  static Future<void> openMap(double latitude, double longitude) async {
+    final String googleMapUrl =
+        'https://www.google.com/maps/search/?api=1&query=$latitude,$longitude';
+
+    try {
+      await launchUrl(Uri.parse(googleMapUrl));
+    } catch (e) {
+      print('Error opening the map: $e');
+      throw 'Could not open the Map';
+    }
   }
 }
