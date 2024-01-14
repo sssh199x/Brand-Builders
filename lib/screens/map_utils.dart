beeccadb0bf3d5563fcc8260303a28c5c8cf9076
Sandesh Hamal Thakuri <sandeshhamal5890@gmail.com>
2024-01-14 15:46:30 +0545

    Introduced new static function to open the dialpad in map_utils and also implemented snackbar using build context.

--- lib/screens/map_utils.dart
+++ lib/screens/map_utils.dart
@@ -1,9 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class MapUtils {
   MapUtils._();
   // Static Function : A  method that defines as a member of an object but is accesible directly from an API objects constructor,rather than from a object instance created via the constructor.
-  static Future<void> openMap(double latitude, double longitude) async {
+  static Future<void> openMap(
+      double latitude, double longitude, BuildContext context) async {
     final String googleMapUrl =
         'https://www.google.com/maps/search/?api=1&query=$latitude,$longitude';
     final parsedUri = Uri.parse(googleMapUrl);
@@ -16,10 +18,62 @@ class MapUtils {
             mode: LaunchMode.inAppBrowserView,
             // To open google map url in new tab i.e _blank
             webOnlyWindowName: '');
+      } else {
+        // To see this working comment added queries in android manidfest file, so that it cant open the url and snackbar is shown instead.
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: const Text(
+                "Couldn't open Google Maps,check if it is installed in your device or not."),
+            action: SnackBarAction(
+              label: 'Retry',
+              onPressed: () async {
+                print('Retrying to open the Google Maps');
+                if (await canLaunchUrl(parsedUri)) {
+                  await launchUrl(parsedUri,
+                      mode: LaunchMode.inAppBrowserView,
+                      // To open google map url in new tab i.e _blank
+                      webOnlyWindowName: '');
+                }
+              },
+            ),
+          ),
+        );
       }
     } catch (e) {
       print('Error opening the map: $e');
       throw 'Could not open the Map';
+    }
+  }
+
+  static Future<void> openDialPad(
+      String contactno, BuildContext context) async {
+    final Uri callLauncherUri = Uri(
+      scheme: 'tel',
+      path: contactno,
+    );
+    try {
+      if (await canLaunchUrl(callLauncherUri)) {
+        await launchUrl(callLauncherUri);
+      } else {
+        // To see this working change tel to tep so that it cant open the url and snackbar is shown instead.
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: const Text("Couldn't open Dialpad"),
+            action: SnackBarAction(
+              label: 'Retry',
+              onPressed: () async {
+                print('Retrying to open the dialpad');
+                if (await canLaunchUrl(callLauncherUri)) {
+                  await launchUrl(callLauncherUri);
+                }
+              },
+            ),
+          ),
+        );
+      }
+    } catch (e) {
+      print('Error opening the dialpad: $e');
+      throw 'Could not open the Dialpad';
     }
   }
 }
