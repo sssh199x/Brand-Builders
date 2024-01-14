4650c862e063cb008c653acbd6cdbb92ba6a0bc3
Sandesh Hamal Thakuri <sandeshhamal5890@gmail.com>
2024-01-14 15:42:33 +0545

    Changed name from foodimage to foodimageforrestro and added new field parameters of latitude and longitude to restaurant_model

--- lib/provider/restaurant_model.dart
+++ lib/provider/restaurant_model.dart
@@ -9,13 +9,15 @@ class RestaurantModel with ChangeNotifier {
       description:
           'French, Cafe, European, Healthy, Vegetarian Friendly, Vegan Options',
       image: 'assets/frenchcrepie.jpg',
-      foodimage: 'assets/frenchcrepiefood1.jpeg',
+      foodimageforrestro: 'assets/frenchcrepiefood1.jpeg',
       rating: 4.5,
       reviews: 232,
       distance: 1.3,
       time: 24,
       location: 'Khahare Chowk, Lakeside',
       contactno: '980-6667705',
+      latitude: 28.2171,
+      longitude: 83.9587,
     ),
     Restaurant(
       restaurantinfo:
@@ -24,13 +26,15 @@ class RestaurantModel with ChangeNotifier {
       description:
           'Healthy and Hygienic fire wood pizza with the traditional wood fire oven.',
       image: 'assets/godfather.jpg',
-      foodimage: 'assets/pizza.jpg',
+      foodimageforrestro: 'assets/pizza.jpg',
       rating: 4.5,
       reviews: 232,
       distance: 1.3,
       time: 24,
       location: 'Lakeside Rd, Hallan Chowk 18th street',
       contactno: '061-466501',
+      latitude: 28.2153,
+      longitude: 83.9585,
     ),
     Restaurant(
       restaurantinfo:
@@ -38,24 +42,28 @@ class RestaurantModel with ChangeNotifier {
       name: 'Byanjan',
       description: 'Local cuisine, Indian, Contemporary, Nepali, International',
       image: 'assets/bhajyan.jpg',
-      foodimage: 'assets/byanjanfood.jpg',
+      foodimageforrestro: 'assets/byanjanfood.jpg',
       rating: 4.5,
       reviews: 232,
       distance: 1.3,
       time: 24,
       location: 'Lakeside Rd 15, Pokhara',
       contactno: '061-456271',
+      latitude: 28.2090,
+      longitude: 83.9571,
     ),
     Restaurant(
       restaurantinfo: 'Cool vibe. Perfect ambience. Delicious food.',
       name: 'Roadhouse Cafe',
       description: 'Italian, Bar, Pizza, European, Diner, Vegetarian Friendly',
       image: 'assets/roadhouse.jpg',
-      foodimage: 'assets/roadhousefood.jpg',
+      foodimageforrestro: 'assets/roadhousefood.jpg',
       rating: 4.5,
       reviews: 232,
       distance: 1.3,
       time: 24, location: 'Hallan chowk ,Lakeside', contactno: '061-457421',
+      latitude: 28.2138,
+      longitude: 83.9574,
       // momos: ['Chicken Momo','Veg Momo'],
       // fooddescription: ['Chicken,Soup & Flour','Smashed Vegetables,Soup & Flour'],
       // foodprice: ['450','350'],
@@ -67,13 +75,15 @@ class RestaurantModel with ChangeNotifier {
       description:
           'French, Cafe, European, Healthy, Vegetarian Friendly, Vegan Options',
       image: 'assets/frenchcrepie.jpg',
-      foodimage: 'assets/frenchcrepiefood1.jpeg',
+      foodimageforrestro: 'assets/frenchcrepiefood1.jpeg',
       rating: 4.5,
       reviews: 232,
       distance: 1.3,
       time: 24,
       location: 'Khahare Chowk, Lakeside',
       contactno: '980-6667705',
+      latitude: 28.2171,
+      longitude: 83.9587,
     ),
   ];
 }
@@ -83,10 +93,10 @@ class Restaurant {
       image,
       description,
       restaurantinfo,
-      foodimage,
+      foodimageforrestro,
       location,
       contactno;
-  final double rating, distance;
+  final double rating, distance, latitude, longitude;
   final int reviews, time;
   //final List momos,fooddescription,foodprice;
 
@@ -94,9 +104,11 @@ class Restaurant {
     // required this.momos,
     // required this.fooddescription,
     // required this.foodprice,
+    required this.latitude,
+    required this.longitude,
     required this.contactno,
     required this.location,
-    required this.foodimage,
+    required this.foodimageforrestro,
     required this.restaurantinfo,
     required this.description,
     required this.name,
