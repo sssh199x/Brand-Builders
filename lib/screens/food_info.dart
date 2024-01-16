c30f25d5ab5b5f04211f9770fd8177d36a4e6773
Sandesh Hamal Thakuri <sandeshhamal5890@gmail.com>
2024-01-16 16:04:02 +0545

    refactored code for providing the image that it should open when clicked

--- lib/screens/food_info.dart
+++ lib/screens/food_info.dart
@@ -1,16 +1,91 @@
 import 'package:flutter/material.dart';
 import 'package:ordering_app/provider/food_model.dart';
-import 'package:ordering_app/screens/portrait_landscape_stack.dart';
 import 'package:provider/provider.dart';
-import 'about_restaurant.dart';
 
 class FoodInfo extends StatelessWidget {
-  final AboutRestaurant restaurant;
-  const FoodInfo({super.key, required this.restaurant});
+  const FoodInfo({super.key, required this.foods});
+  final Food foods;
+  Widget _buildPortraitStackForFoodInfo(
+      BuildContext context, Orientation orientation, double screenWidth) {
+    return Stack(
+      children: [
+        Positioned(
+          top: 0,
+          left: 0,
+          right: 0,
+          child: Align(
+            child: SizedBox(
+              width: double.infinity,
+              height: orientation == Orientation.portrait
+                  ? screenWidth - 160
+                  : screenWidth - 720,
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage(foods.image),
+                      // includeAvatar ? AssetImage(widget.restaurant.foodimageforrestro) : AssetImage(food.foodItems.first.image),
+                      fit: BoxFit.cover),
+                ),
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+            top: 10,
+            left: 10,
+            child: Align(
+              child: SizedBox(
+                width: orientation == Orientation.portrait
+                    ? screenWidth / 10.2857
+                    : screenWidth / 24.2938, // 40 : 35
+                height: orientation == Orientation.portrait
+                    ? screenWidth / 10.2857
+                    : screenWidth / 24.2938,
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.circle,
+                  ),
+                  child: IconButton(
+                      iconSize:
+                          orientation == Orientation.portrait ? 24.0 : 15.0,
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: const Icon(Icons.arrow_back_ios_new)),
+                ),
+              ),
+            )),
+        Positioned(
+            top: 10,
+            right: 10,
+            child: Align(
+              child: SizedBox(
+                width: orientation == Orientation.portrait
+                    ? screenWidth / 10.2857
+                    : screenWidth / 24.2938, // 40 : 35
+                height: orientation == Orientation.portrait
+                    ? screenWidth / 10.2857
+                    : screenWidth / 24.2938,
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.circle,
+                  ),
+                  child: IconButton(
+                      iconSize:
+                          orientation == Orientation.portrait ? 24.0 : 15.0,
+                      onPressed: () {},
+                      icon: const Icon(Icons.search)),
+                ),
+              ),
+            )),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    double paddingSize = MediaQuery.of(context).size.width * 0.04;
     double screenWidth = MediaQuery.of(context).size.width;
     final orientation = MediaQuery.of(context).orientation;
     return Scaffold(body: Consumer<FoodModel>(
@@ -23,13 +98,8 @@ class FoodInfo extends StatelessWidget {
                         top: MediaQuery.of(context).padding.top),
                     width: double.infinity,
                     height: screenWidth - 140,
-                    child: PortraitLandscapeStack(
-                        widget: restaurant,
-                        screenWidth: screenWidth,
-                        context: context,
-                        paddingSize: paddingSize,
-                        orientation: orientation,
-                        includeAvatar: false),
+                    child: _buildPortraitStackForFoodInfo(
+                        context, orientation, screenWidth),
                   )
                 ],
               )
