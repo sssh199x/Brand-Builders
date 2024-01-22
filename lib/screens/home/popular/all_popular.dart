af02366a611657a85ab729b46fe6a23bf3220163
sssh199x <sandeshhamal5890@gmail.com>
2024-01-22 16:09:29 +0545

    fix : made the code of allpopular  responsive for portrait and landscape mode.

--- lib/screens/home/popular/all_popular.dart
+++ lib/screens/home/popular/all_popular.dart
@@ -9,6 +9,10 @@ class PopularAll extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
+    Orientation orientation = MediaQuery.of(context).orientation;
+    double aspectRatio = 9 / 16;
+    double extraHeight = 50;
+    double reduceHeight = 200;
     return Scaffold(
       appBar: AppBar(
           title: const Text('Popular'),
@@ -16,20 +20,33 @@ class PopularAll extends StatelessWidget {
           foregroundColor: bgColor),
       body: Consumer<RestaurantModel>(
         builder: (context, value, child) {
-          return ListView.builder(
-            itemCount: value.restaurants.length,
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: RestaurantView(
-                  restaurant: value.restaurants[index],
-                  sliderHeight: screenWidth * (9 / 16) +
-                      50, // (9/16 = 0.5625, 411.42857 * 0.5625 = 231.428)
-                  sliderWidth: double.infinity,
-                ),
-              );
-            },
-          );
+          return orientation == Orientation.portrait
+              ? ListView.builder(
+                  itemCount: value.restaurants.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: RestaurantView(
+                        restaurant: value.restaurants[index],
+                        sliderHeight: screenWidth * aspectRatio + extraHeight,
+                        sliderWidth: screenWidth,
+                      ),
+                    );
+                  },
+                )
+              : ListView.builder(
+                  itemCount: value.restaurants.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: RestaurantView(
+                        restaurant: value.restaurants[index],
+                        sliderHeight: screenWidth * aspectRatio - reduceHeight,
+                        sliderWidth: screenWidth,
+                      ),
+                    );
+                  },
+                );
         },
       ),
     );
