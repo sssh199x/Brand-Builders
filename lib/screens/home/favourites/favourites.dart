cdbf867ddb999ca02a0a0139ed90c4df33495bb5
sssh199x <sandeshhamal5890@gmail.com>
2024-01-23 16:19:53 +0545

    refactored for landscape mode

--- lib/screens/home/favourites/favourites.dart
+++ lib/screens/home/favourites/favourites.dart
@@ -8,12 +8,11 @@ import 'favourite_view.dart';
 class FavouriteRestaurants extends StatelessWidget {
   const FavouriteRestaurants({super.key});
 
-  Widget _buildSectionHeader(BuildContext context) {
-    double scrWidth = MediaQuery.of(context).size.width;
+  Widget _buildSectionHeader(double screenWidth, BuildContext context) {
     return SectionHeader(
         headerText: 'Your Favourite Restaurants',
         viewText: 'View All  >',
-        viewWidth: scrWidth * 0.2,
+        viewWidth: screenWidth * 0.2,
         onViewClick: () {
           Navigator.push(
             context,
@@ -24,45 +23,80 @@ class FavouriteRestaurants extends StatelessWidget {
         });
   }
 
-  Widget _buildFavouriteRestaurantList(BuildContext context) {
-    double scrWidth = MediaQuery.of(context).size.width;
-    return Container(
-      padding: const EdgeInsets.only(bottom: 16),
-      height: scrWidth < 420 ? scrWidth / 1.8701 : scrWidth / 1.8,
-      child: Consumer<RestaurantModel>(builder: (context, value, child) {
-        int count = value.restaurants.length;
-        return ListView.builder(
-          padding: EdgeInsets.zero,
-          shrinkWrap: true,
-          scrollDirection: Axis.horizontal,
-          itemCount: count > 6 ? 6 : count,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.only(right: 8.0),
-              child: FavouriteView(
-                favItem: value.restaurants[index],
-                sliderWidth: scrWidth - 170,
-                sliderHeight: scrWidth * 9 / 16 - 20,
-              ),
-            );
-          },
-        );
-      }),
-    );
+  Widget _buildFavouriteRestaurantList(
+    Orientation orientation,
+    double sliderWidth,
+    double sliderHeight,
+    double screenWidth,
+  ) {
+    return orientation == Orientation.portrait
+        ? Container(
+            margin: const EdgeInsets.all(5),
+            height: sliderHeight - 110,
+            child: Consumer<RestaurantModel>(builder: (context, value, child) {
+              int count = value.restaurants.length;
+              return ListView.builder(
+                padding: EdgeInsets.zero,
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: count > 6 ? 6 : count,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    child: FavouriteView(
+                      favItem: value.restaurants[index],
+                      sliderWidth: sliderWidth,
+                      sliderHeight: sliderHeight - 100,
+                    ),
+                  );
+                },
+              );
+            }),
+          )
+        : Container(
+            margin: const EdgeInsets.all(15),
+            height: sliderHeight - 130,
+            child: Consumer<RestaurantModel>(builder: (context, value, child) {
+              int count = value.restaurants.length;
+              return ListView.builder(
+                padding: EdgeInsets.zero,
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: count > 6 ? 6 : count,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    child: FavouriteView(
+                        favItem: value.restaurants[index],
+                        sliderWidth: sliderWidth,
+                        sliderHeight: sliderHeight - 110),
+                  );
+                },
+              );
+            }),
+          );
   }
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    Orientation orientation = MediaQuery.of(context).orientation;
+    double sliderWidth = screenWidth > 420
+        ? orientation == Orientation.portrait
+            ? screenWidth / 1.4333
+            : screenWidth / 2.8
+        : screenWidth; //(184.5493:300.0069):screenWidth
+    double sliderHeight = sliderWidth * (9 / 16) + 60; // 301.875
     return Container(
-      padding: EdgeInsets.symmetric(
-          horizontal: MediaQuery.of(context).size.width * 0.04),
+      padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.04),
       child: Column(
         children: [
-          _buildSectionHeader(context),
+          _buildSectionHeader(screenWidth, context),
           const SizedBox(
             height: 10,
           ),
-          _buildFavouriteRestaurantList(context),
+          _buildFavouriteRestaurantList(
+              orientation, sliderHeight, sliderWidth, sliderHeight),
         ],
       ),
     );
