037c211330468887505575f3264411ce87525fa7
sssh199x <sandeshhamal5890@gmail.com>
2024-01-25 16:18:53 +0545

    Add :  new home page

---
+++ lib/screens/home_page_1.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:ordering_app/res/constants.dart';
+
+class HomePage1 extends StatelessWidget {
+  const HomePage1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: prmColor,
+    );
+  }
+}
