7cf58453ee11118e027f317502321c9e62471a88
sssh199x <sandeshhamal5890@gmail.com>
2024-01-26 16:19:07 +0545

    Update: HomePage1

--- lib/screens/home_page_1.dart
+++ lib/screens/home_page_1.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:ordering_app/res/constants.dart';
+import 'package:ordering_app/screens/searchbox_header_1.dart';
 
 class HomePage1 extends StatelessWidget {
   const HomePage1({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: prmColor,
+      body: Container(
+        height: double.infinity,
+        color: const Color.fromARGB(255, 237, 236, 236),
+        //Colors.white.withOpacity(0.8),
+        margin: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+        child: const SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: [
+              BuisnessSearchBoxHeader(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
